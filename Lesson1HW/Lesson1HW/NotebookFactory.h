4bf255961a5a9597d62835993d6103e25171a2f7
datastore24 <co@datastore24.ru>
2015-08-07 08:59:00 +0300

    Lesson 1_homework

---
+++ Lesson1HW/Lesson1HW/NotebookFactory.h
@@ -0,0 +1,22 @@
+//
+//  NotebookFactory.h
+//  Lesson1HW
+//
+//  Created by Кирилл Ковыршин on 07.08.15.
+//  Copyright (c) 2015 datastore24. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NotebookFactory : NSObject
+
+- (void) startFactory;
+- (void) finishFactory;
+- (void) startAssembly;
+- (void) finishAssembly;
+- (void) startPackaging;
+- (void) finishPackaging;
+
+
+
+@end
---
+++ Lesson1HW/Lesson1HW/NotebookFactory.m
@@ -0,0 +1,131 @@
+//
+//  NotebookFactory.m
+//  Lesson1HW
+//
+//  Created by Кирилл Ковыршин on 07.08.15.
+//  Copyright (c) 2015 datastore24. All rights reserved.
+//
+
+#import "NotebookFactory.h"
+
+@implementation NotebookFactory
+
+- (void) startFactory {
+    NSLog(@"Добро пожаловать на фабрику ноутбуков Samsung \n ");
+    NSLog(@"Начинаем изготовление компонентов \n ");
+    [self factoryCase];
+    [self factoryMotherboard];
+    [self factoryHdd];
+    [self factoryRam];
+    [self factoryMatrix];
+    [self factoryModules];
+    
+}
+
+- (void) factoryCase {
+    
+    NSLog(@"Изготовление корпуса - успешно");
+    
+}
+
+- (void) factoryMotherboard {
+    
+    NSLog(@"Изготовление материнской платы - успешно");
+    
+}
+
+- (void) factoryHdd{
+    
+    NSLog(@"Изготовление жестко диска HDD - успешно");
+    
+}
+
+- (void) factoryRam {
+    
+    NSLog(@"Изготовление оперативной памяти - успешно");
+    
+}
+
+- (void) factoryMatrix {
+    
+    NSLog(@"Изготовление LED матрицы - успешно");
+    
+}
+
+- (void) factoryModules {
+    
+    NSLog(@"Изготовление других модулей - успешно");
+    
+}
+
+- (void) finishFactory {
+    
+    NSLog(@"Изготовление компонентов закончено успешно");
+    
+}
+
+- (void) startAssembly {
+    
+    NSLog(@"Компоненты переданы на сборку \n ");
+    NSLog(@"Начинаем сборку \n ");
+    
+    [self installMotherboard];
+    [self installHdd];
+    [self installRam];
+    [self installingMatrix];
+    [self installingModules];
+    
+}
+
+- (void) installMotherboard {
+    
+    NSLog(@"Установка материнской платы в корпус - успешно");
+    
+}
+
+- (void) installHdd {
+    
+    NSLog(@"Установка жесткого диска + подключение - успешно");
+    
+}
+
+- (void) installRam {
+    
+    NSLog(@"Установка оперативной памяти - успешно");
+    
+}
+
+
+- (void) installingMatrix {
+    
+    NSLog(@"Установка LCD матрицы в корпус - успешно");
+    
+}
+
+- (void) installingModules {
+    
+    NSLog(@"Установка других модулей - успешно");
+    
+}
+
+- (void) finishAssembly {
+    
+    NSLog(@"Установка компонентов успешно завершена");
+    
+}
+
+- (void) startPackaging {
+    
+    NSLog(@"Готовая продукция передана на упаковку \n ");
+    NSLog(@"Упаковка готовой продукции начата \n ");
+    
+}
+
+- (void) finishPackaging {
+    
+    NSLog(@"Упаковка готовой продукции закончена успешно \n ");
+    
+}
+
+
+@end
