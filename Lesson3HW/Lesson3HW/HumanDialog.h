16b357a3f6f0d3bf2b69282fe81a58d984efbb59
datastore24 <co@datastore24.ru>
2015-08-15 09:33:38 +0300

    Lesson3 of Vladimir GeekBrains

---
+++ Lesson3HW/Lesson3HW/HumanDialog.h
@@ -0,0 +1,13 @@
+//
+//  HumanDialog.h
+//  Lesson3HW
+//
+//  Created by Кирилл Ковыршин on 15.08.15.
+//  Copyright (c) 2015 datastore24. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HumanDialog : NSObject
+
+@end
---
+++ Lesson3_ex/Lesson3_ex/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lesson3_ex
+//
+//  Created by Кирилл Ковыршин on 13.08.15.
+//  Copyright (c) 2015 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
