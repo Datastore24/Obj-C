3767ce8452ceab3999da2b07d12107979ca2068a
datastore24 <co@datastore24.ru>
2015-08-24 00:08:28 +0300

    Lesson By Vladimir

---
+++ Lesson5HW/Lesson5HW/ShopClass.h
@@ -0,0 +1,13 @@
+//
+//  ShopClass.h
+//  Lesson5HW
+//
+//  Created by Кирилл Ковыршин on 23.08.15.
+//  Copyright (c) 2015 datastore24. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShopClass : NSObject
+
+@end
---
+++ Lesson5_ex/Lesson5_ex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson5_ex
+//
+//  Created by Кирилл Ковыршин on 22.08.15.
+//  Copyright (c) 2015 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
