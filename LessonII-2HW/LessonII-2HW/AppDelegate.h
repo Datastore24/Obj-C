b2aaa3ec73d8b96b359fb859353ab33d014d3b1a
Datastore24Kirill <co@datastore24.ru>
2015-09-16 01:29:25 +0300

    Lesson of Vladimir

---
+++ LessonII-2HW/LessonII-2HW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LessonII-2HW
+//
+//  Created by Кирилл Ковыршин on 15.09.15.
+//  Copyright (c) 2015 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LessonII-2HW/LessonII-2HW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LessonII-2HW
+//
+//  Created by Кирилл Ковыршин on 15.09.15.
+//  Copyright (c) 2015 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
